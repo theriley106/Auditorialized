b33d36589d4a1d57dec21ba60628dd0e52e012c0
Theriley106 <christopherlambert106@gmail.com>
2019-06-11 19:46:26 -0700

    Added better audio downloading/trimming

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import shutil
 import random
+import os
 
 def gen_captcha_image_name():
 	return ''.join([str(random.randint(1, 9)) for i in range(10)])
@@ -10,6 +11,10 @@ def download_image(url, saveAs):
 	with open(saveAs, 'wb') as out_file:
 	    shutil.copyfileobj(response.raw, out_file)
 	del response
+
+def trim_audio(audioFile, seconds=6):
+	os.system('ffmpeg -i {} -ss 6 -vcodec copy -acodec copy {}.mp3'.format(audioFile, "temp"))
+	os.system("mv temp.mp3 {}".format(audioFile))
 
 def download_mp3(url, saveAs):
 	response = requests.get(url, stream=True)
@@ -25,8 +30,11 @@ def gen_new_image_captcha():
 def gen_new_audio_captcha():
 	url = 'https://www.amazon.com/ap/captcha?appAction=SIGNIN&captchaObfuscationLevel=ape%3AaGFyZA%3D%3D&captchaType=audio'
 	res = requests.get(url)
-	download_mp3(res.json().get('captchaImageUrl'), 'captchas/{}.mp3'.format(gen_captcha_image_name()))
+	fileName = 'captchas/{}.mp3'.format(gen_captcha_image_name())
+	download_mp3(res.json().get('captchaImageUrl'), fileName)
+	trim_audio(fileName)
+
 
 if __name__ == '__main__':
 	for i in range(25):
-		gen_new_image_captcha()
+		gen_new_audio_captcha()
